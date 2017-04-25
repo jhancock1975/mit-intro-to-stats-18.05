e43aca34645c079b59e300136fcca33400da5686
John Hancock <jhancock1975@gmail.com>
2017-04-25 07:30:23 -0400

    start on problem set 4 and update script

--- scripts/pdfNPush.sh
+++ scripts/pdfNPush.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-../scripts/pdfBib.sh $1
+if [ $# -ne 1]; then
+	echo "usage pdfNPush.sh <<branchname>>";
+	exit 0;
+fi
+branch_name=$1
+~/scripts/pdfBib.sh $1
 git add .
 git commit -m "compile pdf"
-git push -u origin slides5
+git push -u origin $branch_name
 
